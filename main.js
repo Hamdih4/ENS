83dcfe156087b2e06a7a0907799f76e3293a427b
Hamdi Alhaj <hamdih4@gmail.com>
2017-03-20 21:53:00 -0700

    Add babel build

--- main.js
+++ main.js
@@ -1,59 +1,8 @@
-var cron = require('cron');
-var mailer = require('nodemailer');
+import Mailer from './mailer';
 
-var cronJob;
-var transporter;
+console.log(Mailer);
 
-var iteration = 0;
-var delay = 30;
-var mailerConfig = {
-  service: 'gmail',
-  auth: {
-    user: 'att.sdk.m2x.dev.1@gmail.com',
-    pass: '6haC=xj9<n/$](Sp'
-  }
-};
+//Mailer();
+let mailer = new Mailer();
 
-var contacts = [];
-
-var mailOptions = {
-  to: 'ha702t@gmail.com',
-  subject: 'DVAS tests failed!',
-  html: '<b>Failure Found</b>'
-};
-
-function notifyContacts() {
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.info('Email error');
-      console.trace(error);
-    } else {
-      console.info('Notification sent to contacts');
-      console.trace(info);
-    }
-  });
-}
-
-function checkForFailures() {
-  iteration+=1;
-
-  console.info('check #', iteration);
-  notifyContacts();
-}
-
-(function main() {
-
-  console.log('ENS Server Started');
-  console.info('Check every %d seconds', delay);
-
-  cronJob = cron.job('*/' + delay + ' * * * * *', checkForFailures);
-  transporter = mailer.createTransport(mailerConfig ,{
-    debug: true
-  });
-
-  cronJob.start();
-
-}());
-
-
-
+mailer.sendNotification();
